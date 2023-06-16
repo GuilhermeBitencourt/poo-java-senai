ec9bf22f29b5d7170fd18accc8c82782e9c45352
GuilhermeBitencourt <120538988+GuilhermeBitencourt@users.noreply.github.com>
2023-06-16 11:20:19 -0300

    Add files via upload

---
+++ impressora/Epson.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package impressora;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Epson implements Impressora{
+
+    @Override
+    public void imprimir() {
+        System.out.println("==");
+        System.out.println("");
+        System.out.println("");
+    }
+    
+    
+    
+    
+}
